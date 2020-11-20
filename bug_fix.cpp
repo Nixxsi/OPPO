cc09040a51068367a90ba09788d5b0bdcf5ff855
Nixxsi <57566566+Nixxsi@users.noreply.github.com>
2020-11-20 21:46:07 +0300

    Create Fix Bugs

---
+++ bug_fix.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <clocale>
+
+double my_pow(double, int);
+
+int main()
+{
+    using namespace std;
+    setlocale(LC_ALL, "rus");
+
+    double x;
+    int y;
+    cout << "Введите число и степень..." << endl;
+    cin >> x >> y;
+
+    cout << "Результат вычисления... " << endl << my_pow(x, y) << endl;
+    
+}
+
+double my_pow(double x, int y) {
+
+    double rez = 1.;
+    if (y > 0) {
+        for (int i = 0; i < y; i++) {
+            rez *= x;
+        }
+    }
+    else {
+        for (int i = 0; i < -y; i++) {
+            rez *= x;
+
+        }
+    }
+    return 1 / rez;
+}
